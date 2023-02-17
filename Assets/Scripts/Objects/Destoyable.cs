8f9fe91a2af31727fb28fc8a5eb5b9a943f29078
Klv13 <kristian.vestrup@outlook.dk>
2023-02-17 19:39:17 +0100

    Key added to bryggers

--- Assets/Scripts/Objects/Destoyable.cs
+++ Assets/Scripts/Objects/Destoyable.cs
@@ -6,15 +6,33 @@ public class Destoyable : MonoBehaviour
 {
 
     public GameObject DestroyedVersion;
+    public string tag = "Player";
+    public GameObject Needs;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag.ToString() == "Player")
+        if (other.tag.ToString() == tag)
         {
             Debug.Log("Dør");
             DestroyedVersion.SetActive(true);
             Destroy(gameObject);
             
         }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag.ToString() == tag)
+        {
+            Debug.Log("Dør");
+            DestroyedVersion.SetActive(true);
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            Needs.active = true;
+        }
+
     }
 }
